fde77c0099efffce6d565432f944e39f0e76399d
Anurag Maheshwari <115565191+AM-ROBOTS@users.noreply.github.com>
2024-09-14 12:20:31 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,3 +1,4 @@
+# @AmRobots_Bots www.hostingup.in
 import os
 from os import getenv, environ
 from dotenv import load_dotenv
@@ -16,8 +17,8 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PORT = int(getenv('PORT', 9087)) #(Here, enter the hosting port you received from HostingUp, which should start From 9000)
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0')) #(DO NOT CHANGE IF U DEPLOY YOUR BOT ON HOSTINGUP VPS)
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
@@ -30,7 +31,8 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL', True))
+    HAS_SSL=bool(getenv('HAS_SSL', False))
+    FQDN = 'bots.hostingup.icu:9097' #(Here, enter the domain and hosting port you received from HostingUp. If you're using your own, enter that, but make sure to use the domain name, not the IP address, and include the port as well.)
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
